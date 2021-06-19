50ef61a22c66e5e7ce972442ffe1b5d2d8f08d7d
victor.castillo <victor.castillo@fulltimeforce.com>
2021-06-19 17:44:32 -0500

    Update: changed email address

--- utils/email.js
+++ utils/email.js
@@ -1,59 +1,61 @@
-const nodemailer = require('nodemailer')
-const pug = require('pug')
-const htmlToText = require('html-to-text')
+const nodemailer = require("nodemailer");
+const pug = require("pug");
+const htmlToText = require("html-to-text");
 
-const Email = options => {
+const Email = (options) => {
   const gmail = {
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASS
-    }
-  }
+      pass: process.env.GMAIL_PASS,
+    },
+  };
 
-  const transport = nodemailer.createTransport(gmail)
+  const transport = nodemailer.createTransport(gmail);
 
   // 1) Render HTML based on a pug template
-  const html = pug.renderFile(`${__dirname}/../email/${options.template}.pug`, options.templateData)
+  const html = pug.renderFile(
+    `${__dirname}/../email/${options.template}.pug`,
+    options.templateData
+  );
 
   // Define email options
   let mailOptions = {
-    from: 'Stand For Trees Support <businessbadging@standfortrees.org>',
+    from: "Stand For Trees Support <business@standfortrees.org>",
     to: options.to,
     subject: options.subject,
     text: htmlToText.fromString(html),
     cc: options.cc,
-    html
-  }
+    html,
+  };
 
-  mailOptions.attachments = [{
-    filename: 'fb.png',
-    path: `${__dirname}/../assets/images/fb.png`,
-    cid: 'fb'
-  },
-  {
-    filename: 'tw.png',
-    path: `${__dirname}/../assets/images/tw.png`,
-    cid: 'tw'
-  },
-  {
-    filename: 'ig.png',
-    path: `${__dirname}/../assets/images/ig.png`,
-    cid: 'ig'
-  },
-  {
-    filename: 'logo.png',
-    path: `${__dirname}/../assets/images/logo.png`,
-    cid: 'logo'
-  }]
+  mailOptions.attachments = [
+    {
+      filename: "fb.png",
+      path: `${__dirname}/../assets/images/fb.png`,
+      cid: "fb",
+    },
+    {
+      filename: "tw.png",
+      path: `${__dirname}/../assets/images/tw.png`,
+      cid: "tw",
+    },
+    {
+      filename: "ig.png",
+      path: `${__dirname}/../assets/images/ig.png`,
+      cid: "ig",
+    },
+    {
+      filename: "logo.png",
+      path: `${__dirname}/../assets/images/logo.png`,
+      cid: "logo",
+    },
+  ];
 
-  if (options.attachments)
-    mailOptions.attachments.push(options.attachments)
-
+  if (options.attachments) mailOptions.attachments.push(options.attachments);
 
   //send Email
-  return transport.sendMail(mailOptions)
+  return transport.sendMail(mailOptions);
+};
 
-}
-
-module.exports = Email
+module.exports = Email;
